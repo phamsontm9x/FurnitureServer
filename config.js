f2d18e6fe536adeb199905d0bd0a9550b56ab423
ThanhSon <thanhsoncun@gmail.com>
2018-08-02 21:11:14 +0700

    Init project

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  secret: 'jwtabcxzy?123',
+  url : 'mongodb://admin:admin1@ds111082.mlab.com:11082/furniture',
+  avatarDefaultMale: 'https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?resize=256%2C256&quality=100',
+  avatarDefaultFeMale: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu-r0mf-PrHjeq9w1GH1MN6Ju1GM8ZbBlXVZIZe3GP0q6EuYpv'
+};
